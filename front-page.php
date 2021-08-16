47a5f47eb9556bf2d7353fb76b6628f8230faafa
JbDruet <jb.druet@codeur.online>
2021-08-16 11:43:32 +0200

    saver

--- front-page.php
+++ front-page.php
@@ -1,3 +1,1 @@
 <?php include 'header.php'; ?>
-
-<?php get_template_part( 'testpart' );?>
--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 <?= get_custom_logo($blog_id)?>
 <?php
 wp_nav_menu( array(
-    'theme_location' => 'my-custom-menu', 
+    'theme_location' => 'my-custom-menu',
     'container_class' => 'custom-menu-class',
     'menu_class' => 'nav'
     ));
