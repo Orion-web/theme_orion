3406218c793fe934768d4672ef3d994f7c30bc21
JbDruet <jb.druet@codeur.online>
2021-08-26 16:37:55 +0200

    CarousselOk

--- front-page.php
+++ front-page.php
@@ -58,38 +58,28 @@ if ( $section_bg_img = get_theme_mod( 'section_bg_img' ) )
 
 
 
-<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
 
 
 
-                <?php $loop = new WP_Query((array('post_type' => 'realisations','order'=>'ASC'))); ?>
-                        <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+
+
+<div <?php
+
+                if ( $section_bg_img = get_theme_mod( 'section_bg_img' ) )
+                echo ' style="background-image: url(' . $section_bg_img . ');"';
+
+                        ?>
                         
-                                <?php the_content() ?>
+                        <?php
+                        echo do_shortcode('[smartslider3 slider="2"]');
+                        ?>
+                        
+                        <?php
 
-                        <?php endwhile; wp_reset_query(); ?>
+
+                ?>>
 
 
-    
-<!--       <img class="d-block w-100" src="..." alt="First slide"> -->
-    </div>
-    <div class="carousel-item">
-<!--       <img class="d-block w-100" src="..." alt="Second slide"> -->
-    </div>
-    <div class="carousel-item">
-<!--       <img class="d-block w-100" src="..." alt="Third slide"> -->
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
 </div>
 
 
